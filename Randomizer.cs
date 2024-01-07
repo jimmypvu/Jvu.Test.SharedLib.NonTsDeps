d241aefd0d0509215ef7816993cfe074e15792d0
jimmypvu <114040438+jimmypvu@users.noreply.github.com>
2024-01-07 11:40:09 -0800

    Initial commit

---
+++ Randomizer.cs
@@ -0,0 +1,16 @@
+﻿using AutoBogus;
+
+namespace Jvu.Test.SharedLib.NonTsDeps
+{
+  public class Randomizer
+  {
+    public string RandomString => AutoFaker.Generate<string>();
+    public int RandomInt => AutoFaker.Generate<int>();
+    public bool RandomBool => AutoFaker.Generate<bool>();
+    
+    public T GetRandom<T>()
+    {
+      return AutoFaker.Generate<T>();
+    }
+  }
+}
